26adca7106836e2d5b3b182081c3d55c9ace63c0
xqx333 <141810964+xqx333@users.noreply.github.com>
2024-01-02 16:21:26 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -359,7 +359,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "欢迎使用api.kksj.me提供的gpt聊天服务，如果你是通过配置链接跳转的可以直接使用，或者你也可以在[设置](/#/settings)页修改你喜欢的配置。",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
