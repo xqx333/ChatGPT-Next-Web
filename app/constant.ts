1411986ef9cde5501d11b25e8daefed2b8bc1b1d
xqx333 <141810964+xqx333@users.noreply.github.com>
2024-05-20 05:02:13 +0800

    更新 constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -11,7 +11,7 @@ export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 export const DEFAULT_API_HOST = "https://api.nextchat.dev";
 export const OPENAI_BASE_URL = "https://api.openai.com";
 
-export const GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/";
+export const GEMINI_BASE_URL = "https://kksj.zeabur.app/";
 
 export enum Path {
   Home = "/",
@@ -87,7 +87,7 @@ export const Azure = {
 
 export const Google = {
   ExampleEndpoint: "https://generativelanguage.googleapis.com/",
-  ChatPath: "v1beta/models/gemini-pro:generateContent",
+  ChatPath: "v1/chat/completions",
 
   // /api/openai/v1/chat/completions
 };
