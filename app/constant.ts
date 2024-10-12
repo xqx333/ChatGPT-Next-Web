438e3cd29c14e39aa7f535021322a2bd3ea1b04c
xqx333 <141810964+xqx333@users.noreply.github.com>
2024-10-12 21:21:43 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -271,12 +271,14 @@ export const DEFAULT_TTS_VOICES = [
 ];
 
 const openaiModels = [
-  gpt-3.5-turbo, gpt-3.5-turbo-16k, gpt-3.5-turbo-1106, gpt-3.5-turbo-0125, gpt-4, gpt-4-1106-preview, gpt-4-all, 
-  gpt-4o-all, gpt-4-32k, gpt-4-vision-preview, gpt-4-0125-preview, gpt-4-turbo-preview, gpt-4o-2024-05-13, gpt-4o-2024-08-06, 
-  chatgpt-4o-latest, o1-mini-all, o1-preview-all, dalle-3, claude-3-opus-20240229, claude-3-sonnet-20240229, claude-3-haiku-20240307, 
-  claude-3-5-sonnet-20240620, suno-v3, suno-v3.5, gpt-4-turbo, gpt-4-turbo-2024-04-09, gpt-4o, gpt-4o-mini, gemini-1.5-pro-latest, stable-diffusion, 
-  stable-diffusion-3-2b, playground-v2.5, mj-chat, flux, gpt-4o-2024-08-06, luma-video, luma-vip-video, luma-vip-extend, luma-extend, luma-task, 
-  luma-vip-task, cogvideox, cogvideox-result, runway-video, runway-video-task, vidu-video, vidu-task-get, vidu-video-stable, "o1-mini", "o1-preview",
+  "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-0125", "gpt-4", "gpt-4-1106-preview",
+  "gpt-4-all", "gpt-4o-all", "gpt-4-32k", "gpt-4-vision-preview", "gpt-4-0125-preview", "gpt-4-turbo-preview", 
+  "gpt-4o-2024-05-13", "gpt-4o-2024-08-06", "chatgpt-4o-latest", "o1-mini-all", "o1-preview-all", "dalle-3", 
+  "claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307", "claude-3-5-sonnet-20240620", 
+  "suno-v3", "suno-v3.5", "gpt-4-turbo", "gpt-4-turbo-2024-04-09", "gpt-4o", "gpt-4o-mini", "gemini-1.5-pro", 
+  "stable-diffusion", "stable-diffusion-3-2b", "playground-v2.5", "mj-chat", "flux", "gpt-4o-2024-08-06", 
+  "luma-video", "luma-vip-video", "luma-vip-extend", "luma-extend", "luma-task", "luma-vip-task", "cogvideox", "cogvideox-result",
+  "runway-video", "runway-video-task", "vidu-video", "vidu-task-get", "vidu-video-stable", "o1-mini", "o1-preview"
 ];
 
 // 为其他常量保留占位值
