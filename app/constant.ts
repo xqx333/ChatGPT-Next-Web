dde1416f156605da079d2f30a1c763ca0d05e03d
xqx333 <141810964+xqx333@users.noreply.github.com>
2024-11-24 16:26:12 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -273,7 +273,7 @@ export const DEFAULT_TTS_VOICES = [
 const openaiModels = [ 
    "advanced-voice", "chatgpt-4o-latest", "claude-3-5-sonnet-20240620", "claude-3-5-sonnet-20241022", 
    "claude-3-5-sonnet-all", "claude-3-haiku-20240307", "claude-3-opus-20240229", "claude-3-sonnet-20240229", 
-   "cogvideox", "cogvideox-result", "dalle-3", "dall-e-3", "flux", "gemini-1.5-pro", "gpt-3.5-turbo", 
+   "cogvideox", "cogvideox-result", "dalle-3", "dall-e-3", "flux", "gemini-1.5-flash-latest", "gemini-1.5-pro-latest", "glm-4-flash", "gpt-3.5-turbo", 
    "gpt-3.5-turbo-0125", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-0125-preview", 
    "gpt-4-1106-preview", "gpt-4-32k", "gpt-4-all", "gpt-4-turbo", "gpt-4-turbo-2024-04-09", "gpt-4-turbo-preview", 
    "gpt-4-vision-preview", "gpt-4o", "gpt-4o-2024-05-13", "gpt-4o-2024-08-06", "gpt-4o-2024-08-06", "gpt-4o-all","gpt-4o-mini", "o1-mini", "o1-mini-all", "o1-preview", "o1-preview-all",
