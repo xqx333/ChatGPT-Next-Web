f43234b73333fb16c5c0db7f6a19db5d870b7909
xqx333 <141810964+xqx333@users.noreply.github.com>
2024-12-06 21:40:39 +0800

    更新 constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -276,7 +276,7 @@ const openaiModels = [
    "cogvideox", "cogvideox-result", "dalle-3", "dall-e-3", "flux", "gemini-1.5-flash-latest", "gemini-1.5-pro-latest", "glm-4-flash", "gpt-3.5-turbo", 
    "gpt-3.5-turbo-0125", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-0125-preview", 
    "gpt-4-1106-preview", "gpt-4-32k", "gpt-4-all", "gpt-4-turbo", "gpt-4-turbo-2024-04-09", "gpt-4-turbo-preview", 
-   "gpt-4-vision-preview", "gpt-4o", "gpt-4o-2024-05-13", "gpt-4o-2024-08-06", "gpt-4o-2024-08-06", "gpt-4o-all","gpt-4o-mini", "o1-mini", "o1-mini-all", "o1-preview", "o1-preview-all",
+   "gpt-4-vision-preview", "gpt-4o", "gpt-4o-2024-05-13", "gpt-4o-2024-08-06", "gpt-4o-2024-11-20", "gpt-4o-all","gpt-4o-mini", "o1-mini", "o1-mini-all", "o1-preview", "o1-preview-all",
     "luma-extend", "luma-task", "luma-video", "luma-vip-extend", "luma-vip-task", "luma-vip-video", 
    "mj-chat",  "playground-v2.5", "runway-video", 
    "runway-video-task", "stable-diffusion", "stable-diffusion-3-2b", "suno-v3", "suno-v3.5", "vidu-task-get", 
