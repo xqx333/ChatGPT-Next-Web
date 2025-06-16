528561b3c75dcd8dde701a300ea8daadb960b917
xqx333 <141810964+xqx333@users.noreply.github.com>
2025-06-17 04:03:03 +0800

    Update utils.ts

--- app/utils.ts
+++ app/utils.ts
@@ -281,6 +281,7 @@ export function getMessageImages(message: RequestMessage): string[] {
 }
 
 export function isVisionModel(model: string) {
+  return true;
   const visionModels = useAccessStore.getState().visionModels;
   const envVisionModels = visionModels?.split(",").map((m) => m.trim());
   if (envVisionModels?.includes(model)) {
